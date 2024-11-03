a84accd5f4980a63505f38f4d4b1d1a3d9fd1046
1bitgodot <symbolicon@live.com>
2024-11-04 02:42:20 +0800

    Module 1-3 Updated

---
+++ INSTRUCTOR.md
@@ -0,0 +1,3 @@
+Instructor Name: Phillip Madden<br />
+Email: symbolicon@live.com<br />
+Repository: https://github.com/NewriseLearning/ASP520<br />
---
+++ bak/Module2/Newrise/Services/EventDataService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newrise.Shared.Models;
+
+namespace Newrise.Services {
+	public class EventDataService {
+		private readonly IDbContextFactory<NewriseDbContext> _dcFactory;
+
+		public EventDataService(IDbContextFactory<NewriseDbContext> dcFactory) {
+			_dcFactory = dcFactory;
+		}
+
+		public async Task AddEventAsync(Event item) {
+			using (var dc = await _dcFactory.CreateDbContextAsync()) {
+				await dc.Events.AddAsync(item);
+				await dc.SaveChangesAsync();
+			}
+		}
+
+		public async Task RemoveEventAsync(string id) {
+			using (var dc = await _dcFactory.CreateDbContextAsync()) {
+				var item = await dc.Events.FindAsync(id);
+				if (item != null) {
+					dc.Events.Remove(item);
+					await dc.SaveChangesAsync();
+				}
+			}
+		}
+
+		public async Task UpdateEventAsync(Event item) {
+			using (var dc = await _dcFactory.CreateDbContextAsync()) {
+				var entry = dc.Events.Attach(item);
+				entry.State = EntityState.Modified;
+				await dc.SaveChangesAsync();
+			}
+		}
+
+		public async Task<List<Event>> GetEventsAsync() {
+			using (var dc = await _dcFactory.CreateDbContextAsync())
+				return await dc.Events.ToListAsync();
+		}
+	}
+}
