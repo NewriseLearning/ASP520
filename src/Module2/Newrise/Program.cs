2364336be029df929b555bffad6b7ee4e12eb856
Phillip Madden <symbolicon@live.com>
2024-11-04 17:53:43 +0800

    Module 2 Updated

---
+++ src/Module2/Newrise/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
+using Newrise.Components;
+using Newrise.Services;
+
+namespace Newrise {
+	public class Program {
+		public const string AppName = "Newrise";
+		public static void Main(string[] args) {
+			var builder = WebApplication.CreateBuilder(args);
+			var services = builder.Services;
+			services
+				.AddRazorComponents()
+				.AddInteractiveServerComponents();
+			services.AddMemoryCache();
+			services.AddMudServices();
+			services.AddSingleton<OfficeListProvider>();
+
+			var connectionString = @"Server=.;Database=NewriseDb;TrustServerCertificate=True;Trusted_Connection=True";
+			services.AddDbContextFactory<NewriseDbContext>(
+				options => options.UseSqlServer(connectionString));
+
+			var app = builder.Build();
+			var env = app.Environment;
+			if (env.IsDevelopment()) {
+			}
+			else {
+				app.UseHsts();
+			}
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseAntiforgery();
+			app
+				.MapRazorComponents<App>()
+				.AddInteractiveServerRenderMode();
+			app.Run();
+		}
+	}
+}
