149360f793155765eb59282257643856395e21be
Phillip Madden <symbolicon@live.com>
2024-11-04 13:03:43 +0800

    Module 1 Completed

--- rsc/Module1/Newrise/Program.cs
+++ rsc/Module1/Newrise/Program.cs
@@ -1,14 +1,16 @@
+using MudBlazor.Services;
 using Newrise.Components;
 
 namespace Newrise {
 	public class Program {
+		public const string AppName = "Newrise";
 		public static void Main(string[] args) {
 			var builder = WebApplication.CreateBuilder(args);
 			var services = builder.Services;
 			services
 				.AddRazorComponents()
 				.AddInteractiveServerComponents();
-
+			services.AddMudServices();
 			var app = builder.Build();
 			var env = app.Environment;
 			if (env.IsDevelopment()) {
