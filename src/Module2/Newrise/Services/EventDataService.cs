56bcb44bd7af1b680d95745a3b46fe19bc43fe8e
Phillip Madden <symbolicon@live.com>
2024-11-05 10:16:43 +0800

    Module 2 completed

---
+++ src/Module2/Newrise/Services/EventDataService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newrise.Shared.Models;
+using static MudBlazor.CategoryTypes;
+
+namespace Newrise.Services {
+	public class EventDataService {
+		readonly IDbContextFactory<NewriseDbContext> _dcFactory;
+		readonly ILogger<EventDataService> _logger;
+
+		public EventDataService(
+			IDbContextFactory<NewriseDbContext> dcFactory,
+			ILogger<EventDataService> logger) {
+			_dcFactory = dcFactory;
+			_logger = logger;
+		}
+
+		public async Task AddEventAsync(Event item) {
+			using (var dc = await _dcFactory.CreateDbContextAsync()) {
+				await dc.Events.AddAsync(item);
+				await dc.SaveChangesAsync();
+			}
+		}
+
+		public async Task RemoveEventAsync(string id) {
+			using (var dc = await _dcFactory.CreateDbContextAsync()) {
+				var item = await dc.Events.FindAsync(id);
+				if (item != null) {
+					dc.Events.Remove(item);
+					await dc.SaveChangesAsync();
+				}
+			}
+		}
+
+		public async Task UpdateEventAsync(Event item) {
+			using (var dc = await _dcFactory.CreateDbContextAsync()) {
+				var entry = dc.Events.Attach(item);
+				entry.State = EntityState.Modified;
+				await dc.SaveChangesAsync();
+			}
+		}
+
+		public async Task<List<Event>> GetEventsAsync() {
+			using (var dc = await _dcFactory.CreateDbContextAsync()) {
+				return await dc.Events.ToListAsync();
+			}
+		}
+	}
+}
---
+++ src/Module3/Newrise.Shared/Models/EventType.cs
@@ -0,0 +1,9 @@
+﻿namespace Newrise.Shared.Models {
+	public enum EventType {
+		None = 0,	// Invalid event type (for validation)
+		Presentation,
+		Training,
+		Workshop,
+		Forum
+	}
+}
